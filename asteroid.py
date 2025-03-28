fa9cd5adf2024ec04cb6d6e1d7597ef6544cd8aa
TheCiaphasCain <ualexe01@gmail.com>
2025-03-28 00:27:34 -0700

    completed asteroid split method and shot detection

--- asteroid.py
+++ asteroid.py
@@ -1,17 +1,44 @@
 import pygame
+import random
+import constants
 from circleshape import CircleShape
+
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        super().__init__(x,y,radius)
-       
+        super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(screen,"orange", (self.position.x,self.position.y), self.radius,2)
+        pygame.draw.circle(screen, "orange", self.position, self.radius, 2)
 
-    def update(self,dt):
-        self.position+=(self.velocity * dt)
+    def update(self, dt):
+        self.position += self.velocity * dt
 
+    def split(self):
+        self.kill()
+        if self.radius<= constants.ASTEROID_MIN_RADIUS:
+            return 
+        rand_angle = random.uniform(20, 50)
+        pos_vector = pygame.math.Vector2(self.velocity) #create copies of the velocity vector
+        neg_vector = pygame.math.Vector2(self.velocity)
 
+        pos_vector = pos_vector.rotate(rand_angle) #rotates the copies
+        neg_vector = neg_vector.rotate(-rand_angle)
 
+        pos_vector *= 1.2 #scale copies by 1.2
+        neg_vector *= 1.2
+        new_rad = self.radius - constants.ASTEROID_MIN_RADIUS
+
+        new_asteroid1 = Asteroid(self.position.x,self.position.y,new_rad)
+        new_asteroid2 = Asteroid(self.position.x,self.position.y,new_rad)
+
+        new_asteroid1.velocity = pos_vector
+        new_asteroid2.velocity = neg_vector
         
+        
+
+    
+
+
+
+    
--- main.py
+++ main.py
@@ -1,61 +1,57 @@
-import pygame 
 import sys
+import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
+
+
 def main():
-    pygame.init() #initializes pygame
-    print("Starting Asteroids!")
-    print("Screen width: 1280 \nScreen height: 720")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
-    clock = pygame.time.Clock() #initializes fps clock
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #sets window size
-    updatable = pygame.sprite.Group() #creates a group of objects that can be updated
 
-    drawable = pygame.sprite.Group() #creates a group of objects that can be drawn
-
-    enemy = pygame.sprite.Group()
-
-    AsteroidField.containers = (updatable)
-
-    Asteroid.containers = (enemy,drawable,updatable)
-
-    Player.containers = (updatable, drawable) #adds player object to both groups
-
-
-    Asteroid_field = AsteroidField()
-
-    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
-    
-
-    running = True
-    while running: # this is the "game loop". it runs until the user quits, and keeps the program running
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-        screen.fill("black")
-        for sprite in drawable:
-            sprite.draw(screen) #this sets the solid color of the gui window
+                return
+
         updatable.update(dt)
-        for asteroid_sprite in enemy:
-            if asteroid_sprite.collision(player) == True:
-                print("game over!")
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
                 sys.exit()
-        pygame.display.flip() # this updates what pygame draws to the screen
-        clock.tick(60) #clock sets the fps, in this case to 60
-        dt = clock.tick(60)/1000 #this decouples the game physics from the rendering speed 
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.split(), shot.kill()
 
+        screen.fill("black")
 
+        for obj in drawable: 
+            obj.draw(screen)
 
+        pygame.display.flip()
 
-
-
-
-
-
-
+        # limit the framerate to 60 FPS
+        dt = clock.tick(80) / 1000
 
 
 if __name__ == "__main__":
